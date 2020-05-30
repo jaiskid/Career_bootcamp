855f55a78e53b5975f7a972c27a1463d163a4127
Jaiskid <neeraj001jaiswal@gmail.com>
2020-05-31 04:17:29 +0530

    codeupdate

---
+++ Kartik_bhaiya asgn/happystring.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <functional>
+#include <deque>
+#include <bitset>
+#include <climits>
+#include <cstdio>
+#include <list>
+#include <iomanip>
+using namespace std;
+
+#define ll long long int
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define si second
+#define fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define AS 200001
+#define mod 1000000007
+string longestDiverseString(int a, int b, int c, char aa = 'a', char bb = 'b', char cc = 'c') {
+	if (a < b)
+		return longestDiverseString(b, a, c, bb, aa, cc);
+	if (b < c)
+		return longestDiverseString(a, c, b, aa, cc, bb);
+	if (b == 0)
+		return string(min(2, a), aa);
+	auto use_a = min(2, a), use_b = a - use_a >= b ? 1 : 0;
+	return string(use_a, aa) +  string(use_b, bb) +
+	       longestDiverseString(a - use_a, b - use_b, c, aa, bb, cc);
+}
+int main() {
+	fastIO
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int a, b, c;
+	cin >> a >> b >> c;
+	string ans = " ";
+	ans = longestDiverseString(a, b, c);
+	cout << ans;
+}
